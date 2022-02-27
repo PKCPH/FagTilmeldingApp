3c7643329f99366bfb9c8cc8ef22bf368d4aa509
Patrick CPH <patrick@klingaa.dk>
2022-02-27 19:23:34 +0100

    2nd push for 1st init

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿//Iteration1_Arv branch
+
+Console.WriteLine("Angiv skole: ");
+string? schoolName = Console.ReadLine();
+
+Console.WriteLine("Angiv hovedforløb: ");
+string? schoolCourse = Console.ReadLine();
+
+Console.WriteLine($"{schoolName}, {schoolCourse} fag tilmeldning app.");
+
+Console.ReadKey();
